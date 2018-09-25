e110c494c6c6d06af1b883175195aec8856dac34
Guilherme <guilherme.saraiva@impulsoti.com.br>
2018-09-25 19:12:36 -0300

    Fix Foto Prod e User

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   validates_presence_of :nome
   validates_presence_of :codigo
 
-  has_attached_file :imagem
+  has_attached_file :imagem, :styles => { :original => "400x400>" }
   do_not_validate_attachment_file_type :imagem
   #validates_attachment_presence :imagem, :message => "É necessário enviar a placa do veículo"
   #validates_attachment_content_type :imagem, :message => "O arquivo enviado não é uma imagem", :content_type => %w( image/jpeg image/png image/gif image/pjpeg image/x-png )
