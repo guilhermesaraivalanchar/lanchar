b52e2388070d0b7896643997103785340906ed05
Guilherme <guilherme.saraiva@impulsoti.com.br>
2018-10-29 13:05:42 -0300

    .

--- app/models/escola.rb
+++ app/models/escola.rb
@@ -10,4 +10,18 @@ class Escola < ApplicationRecord
 	has_many :tipo_users
 	has_many :fornecedores
 
+	def saldo_devedor
+		valor_vendido = self.transferencias.where("transferencias.valor > 0").where(tipo: ["VENDA", "VENDA_DIRETA"]).sum(:valor).to_d
+		valor_reembolso = self.transferencias.where("transferencias.valor > 0").where(tipo: ["REEMBOLSO"]).sum(:valor).to_d
+		valor = valor_vendido - valor_reembolso
+		return valor * 0.02
+	end
+
+	def faturamento_diario
+		self.transferencia_gerais.where("transferencia_gerais.created_at > ? AND transferencia_gerais.created_at < ? AND transferencia_gerais.valor > 0", Time.now.beginning_of_day, Time.now.end_of_day).where(cancelada: [nil, false], tipo: ["VENDA", "VENDA_DIRETA"]).sum(:valor)
+	end
+
+	def faturamento_mensal
+		self.transferencia_gerais.where("transferencia_gerais.created_at > ? AND transferencia_gerais.created_at < ? AND transferencia_gerais.valor > 0" , Time.now.beginning_of_month, Time.now.end_of_month).where(cancelada: [nil, false], tipo: ["VENDA", "VENDA_DIRETA"]).sum(:valor)
+	end
 end
