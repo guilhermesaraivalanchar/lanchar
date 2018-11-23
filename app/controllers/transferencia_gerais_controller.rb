c6d1295d100f7e2fca647bdb7574b231db1db39b
Guilherme <guilherme.saraiva@impulsoti.com.br>
2018-11-23 15:36:43 -0200

    .

--- app/controllers/transferencia_gerais_controller.rb
+++ app/controllers/transferencia_gerais_controller.rb
@@ -54,7 +54,7 @@ class TransferenciaGeraisController < ApplicationController
 
   def index
     @can_cancelar = current_user.tem_permissao("cancelar_transacao")
-    @transferencia_gerais = TransferenciaGeral.where(tipo: ["VENDA","VENDA_DIRETA","ENTRADA","SAIDA"], escola_id: current_user.escola_id).order("created_at DESC")
+    @transferencia_gerais = TransferenciaGeral.where("transferencia_gerais.created_at > ? AND transferencia_gerais.created_at < ? AND transferencia_gerais.valor > 0", Time.now.beginning_of_day, Time.now.end_of_day).where(tipo: ["VENDA","VENDA_DIRETA","ENTRADA","SAIDA"], escola_id: current_user.escola_id).order("created_at DESC")
   end
 
   def cancelar_transferencia
